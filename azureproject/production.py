848c9f7f622c6bde1dae1e004a6a48d3b9076614
thomastruong158 <130467940+thomastruong158@users.noreply.github.com>
2023-04-22 17:56:44 +0200

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -8,7 +8,7 @@ CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
-conn_str = '13b2488f4e08c73b1ebf72a4a6774df2da8ee00a8c1f7ef780444b4902222cc9'
+conn_str = "13b2488f4e08c73b1ebf72a4a6774df2da8ee00a8c1f7ef780444b4902222cc9"
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
